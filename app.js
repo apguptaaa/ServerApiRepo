cc576b207bce4c6c182c46c253fc7249aae9ddb3
apguptaaa <guptaap783@gmail.com>
2025-06-29 21:18:13 +0530

    port2

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ cloudinary.config({
   api_key: '129264287522167', 
   api_secret: 'r_W_mc5cI544aSDc_mq7ucukfss' // Click 'View API Keys' above to copy your API secret
 });
-const PORT = process.env.port || 5000;
+const port = process.env.port || 5000;
 
 
 // const PORT = process.env.PORT || 5000
